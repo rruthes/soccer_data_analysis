6a21b05fd04d7870ce02a511d87a36eef1a7d583
rruthes <98650335+rruthes@users.noreply.github.com>
2024-09-07 15:40:18 -0300

    adding fixtures raw files

--- notebooks/fixtures_data_exploration.py
+++ notebooks/fixtures_data_exploration.py
@@ -2,33 +2,17 @@ import requests
 import pandas as pd
 
 url = "https://v3.football.api-sports.io/fixtures"
+
+params={
+    'league': '71',
+    'season': '2022',
+}
 headers = {
   'x-rapidapi-key': '8d466e7fd56a96fade2767ae8de95e4b',
   'x-rapidapi-host': 'v3.football.api-sports.io'
 }
 
-params = {
-    'league': '71',
-    'season': '2022',
-    'page': 1  
-}
+response = requests.request("GET", url, headers=headers, params=params)
 
-all_fixtures = []
-
-while True:
-    response = requests.get(url, headers=headers, params=params)
-    data = response.json()
-
-    all_fixtures.extend(data['response'])
-
-    if data['paging']['current'] == data['paging']['total']:
-        break
-
-#Como o número de dados era muito extenso, foi necessário dividir a coleta em páginas
-params['page'] += 1
-
-df3 = pd.DataFrame(all_fixtures)
-
-print(df3.head())
-
+df3 = pd.DataFrame(response.json()['response'])
 df3.to_csv('fixtures_brasileirao_2024.csv', index=False)
