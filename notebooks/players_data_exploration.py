29fd9ae260e2c98abc7a364e1441b92e4df7821b
Rafael Ruthes <rafaelruthes444@gmail.com>
2024-09-09 12:09:14 -0300

    fix

--- notebooks/players_data_exploration.py
+++ notebooks/players_data_exploration.py
@@ -29,17 +29,3 @@ while True:
 
 df2 = pd.json_normalize(all_players)
 df2.to_csv('players_brasileirao_2022.csv', index=False)
-import csv
-
-input_file = 'players_brasileirao_2022.csv'
-output_file = 'players_brasileirao_2022_cleaned.csv'
-
-with open(input_file, 'r', encoding='utf-8') as infile, open(output_file, 'w', newline='', encoding='utf-8') as outfile:
-    reader = csv.reader(infile)
-    writer = csv.writer(outfile)
-    
-    for row in reader:
-        cleaned_row = [col.strip() for col in row]
-        writer.writerow(cleaned_row)
-
-print(f"Cleaned data has been written to {output_file}")
